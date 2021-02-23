a10bddc98e6d4adc3f74cee5db19fe667631f220
calumpetergunn <calumgunn@gmail.com>
2021-02-23 22:47:32 +0000

    initial commit

---
+++ my_functions.py
@@ -0,0 +1,12 @@
+def greet(name, time_of_day):
+    return "Good " + time_of_day + ", " + name
+
+name_1 = "Bob"
+time_of_day1 = "morning"
+greeting = greet(name_1, time_of_day1)
+print(greeting)
+
+name_2 = "Ada"
+time_of_day2 = "evening"
+greeting = greet(name_2, time_of_day2)
+print(greeting)
