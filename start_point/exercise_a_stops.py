7d85c29beee539f25b69fcf4d67aac94bc31ae27
calumpetergunn <calumgunn@gmail.com>
2021-02-24 10:24:14 +0000

    added answers for a b

--- start_point/exercise_a_stops.py
+++ start_point/exercise_a_stops.py
@@ -3,23 +3,44 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 #1. Add "Edinburgh Waverley" to the end of the liststops.append("Edinburgh Waverley")
 stops.append("Edinburgh Waverley")
+#correct
+
 #2. Add "Glasgow Queen St" to the start of the list
 stops.insert(0, "Glasgow Queen St")
+#correct
+
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 stops.insert(4, "Polmont")
+#correct
+
 #4. Print out the index position of "Linlithgow"
 print(stops.index("Linlithgow"))
+#correct
+
 #5. Remove "Livingston" from the list using its name
 stops.remove("Livingston")
+#correct
+
 #6. Delete "Cumbernauld" from the list by index
 stops.pop(2)
+# or stops.pop(stops.index("Cumbernauld")) #
+# Alternatively use the del operator
+# del stops[stops.index("Cumbernauld")]
+
 #7. Print the number of stops there are in the list
 num_stops = len(stops)
 print(num_stops)
+#sure why not
+
 #8. Sort the list alphabetically
 stops = sorted(stops)
+# stops.sort()
+
 #9. Reverse the positions of the stops in the list
 stops.reverse()
+#correct
+
 #10 Print out all the stops using a for loop
 for stop in stops:
     print(stop)
+# bing bang boom
--- start_point/exercise_b_users.py
+++ start_point/exercise_b_users.py
@@ -58,28 +58,41 @@ users = {
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 print(users["Jonathan"]["twitter"])
+#don't need print command for this or the other first 5
 
 # 2. Get Erik's hometown
 print(users["Erik"]["home_town"])
+# correct
 
 # 3. Get the array of Erik's lottery numbers
 print(users["Erik"]["lottery_numbers"])
+# correct
 
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
+# correct
 
 # 5. Get the smallest of Erik's lottery numbers
 print(users["Erik"]["lottery_numbers"][2])
+# sorted(users["Erik"]["lottery_numbers"])[0]
 
 # 6. Return an array of Avril's lottery numbers that are even
 print(users["Avril"]["lottery_numbers"][0], users["Avril"]["lottery_numbers"][1], users["Avril"]["lottery_numbers"][3]) #there must be a tidier way
+# lottery_numbers = users["Avril"]["lottery_numbers"]
+# even_numbers = []
+# for lottery_number in lottery_numbers:
+#     if lottery_number % 2 == 0:
+#         even_numbers.append(lottery_number)
+
+# print(even_numbers)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"]["lottery_numbers"] = 7 #not correct - have replaced all items with 7 rather than added
+# users["Erik"]["lottery_numbers"].append(7)
 
 # 8. Change Erik's hometown to Edinburgh
 users["Erik"]["home_town"] = "Edinburgh"
-print(users["Erik"]["home_town"])
+#correct!
 
 # 9. Add a pet dog to Erik called "Fluffy"
 users["Erik"]["pets"] = {
@@ -87,6 +100,19 @@ users["Erik"]["pets"] = {
   "species": "dog"
 }
 print(users["Erik"]["pets"]) #once again, has replaced all pets rather than adding
+# users["Erik"]["pets"].append({ "name": "Fluffy", "species": "dog" })
+
 
 # 10. Add another person to the users dictionary
 # unsure how to add 
+# users["ian"] = {
+#     "twitter": "twitter name",
+#     "lottery_numbers": [4,5,6,7,8],
+#     "home_town": "Edinburgh",
+#     "pets": [
+#       {
+#         "name": "Dave",
+#         "species": "dog"
+#       }
+#     ]
+# }
